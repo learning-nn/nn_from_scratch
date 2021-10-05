1a851550241cd5471d92e808350390cf6e930c0f
afmjoaa <mohimenul.joaa@bkash.com>
2021-10-05 23:36:01 +0600

    NESA-0004: Calculate the layer output using dot product

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # converting to a layer with 4 input and 3 neuron
 inputs = [1.2, 2.1, 3.4, 1.2]
 weights = [[4.1, -4.5, 3.1, 2.3],
@@ -5,12 +7,5 @@ weights = [[4.1, -4.5, 3.1, 2.3],
            [4.1, 4.5, 3.1, -2.3]]
 biases = [1, 2, 3]
 
-layer_outputs = []
-for neuron_weight, neuron_bias in zip(weights, biases):
-    neuron_output = 0
-    for n_input, weight in zip(inputs, neuron_weight):
-        neuron_output += n_input*weight
-    neuron_output += neuron_bias
-    layer_outputs.append(neuron_output)
-
+layer_outputs = np.dot(weights, inputs) + biases
 print(layer_outputs)
