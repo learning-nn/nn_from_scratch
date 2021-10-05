f924c2cfcad9760dd9fbbb28710c2ae02901ec66
afmjoaa <mohimenul.joaa@bkash.com>
2021-10-05 22:54:00 +0600

    NESA-0002: Converted the neuron into a layer

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-inputs = [1.2, 2.1, 3.4]
-weights = [4.1, 4.5, 3.1]
-bias = 2
+# converting to a layer with 4 input and 3 neuron
+inputs = [1.2, 2.1, 3.4, 1.2]
+weight1 = [4.1, -4.5, 3.1, 2.3]
+weight2 = [-4.1, 4.5, 2.1, 2.3]
+weight3 = [4.1, 4.5, 3.1, -2.3]
+bias1 = 1
+bias2 = 2
+bias3 = 3
 
-output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
+output = [
+    inputs[0] * weight1[0] + inputs[1] * weight1[1] + inputs[2] * weight1[2] + inputs[3] * weight1[3] + bias1,
+    inputs[0] * weight2[0] + inputs[1] * weight2[1] + inputs[2] * weight2[2] + inputs[3] * weight2[3] + bias2,
+    inputs[0] * weight3[0] + inputs[1] * weight3[1] + inputs[2] * weight3[2] + inputs[3] * weight3[3] + bias3,
+    ]
 print(output)
