3561c321681dd8e8d67370ca177eae154730738d
afmjoaa <mohimenul.joaa@bkash.com>
2021-10-09 11:50:09 +0600

    NESA-0011: Implemented the simple loss function

--- 2.layer_object.py
+++ 2.layer_object.py
@@ -28,6 +28,25 @@ class ActivationSoftmax:
         self.output = probabilities
 
 
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+
+class LossCategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+
 X, y = spiral_data(100, 3)
 
 dense1 = LayerDense(2, 3)
@@ -41,4 +60,8 @@ activation1.forward(dense1.output)
 dense2.forward(activation1.output)
 activation2.forward(dense2.output)
 
+loss_function = LossCategoricalCrossEntropy()
+loss = loss_function.calculate(activation2.output, y)
+
 print(activation2.output[:5])
+print('Loss:', loss)
